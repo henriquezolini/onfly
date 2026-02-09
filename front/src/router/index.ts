26a4459f484d1a5180f3716b18e0ed2d3a54f62a
Henrique Zolini <henriquezolini@gmail.com>
2026-02-09 15:03:55 -0300

    feat: implementa rotas do front

---
+++ front/src/router/index.ts
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import LoginView from "../views/LoginView.vue";
+import DashboardView from "../views/DashboardView.vue";
+import RequestFormView from "../views/RequestFormView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+      meta: { guest: true }
+    },
+    {
+      path: "/",
+      name: "dashboard",
+      component: DashboardView,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/create-request",
+      name: "create-request",
+      component: RequestFormView,
+      meta: { requiresAuth: true }
+    }
+  ]
+});
+
+router.beforeEach((to, _from, next) => {
+  const authStore = useAuthStore();
+
+  if (to.meta.requiresAuth && !authStore.token) {
+    next({ name: "login" });
+  } else if (to.meta.guest && authStore.token) {
+    next({ name: "dashboard" });
+  } else {
+    next();
+  }
+});
+
+export default router;
